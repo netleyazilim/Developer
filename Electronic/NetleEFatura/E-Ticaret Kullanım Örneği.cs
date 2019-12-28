d89a94100cfc3ed744dec3ca9fe9731882afd134
Netle Yazilim (Netle Global) <goksel.ucer@netle.com.tr>
2019-12-29 01:01:53 +0300

    Create E-Ticaret Kullanım Örneği.cs
    NetleEFatura ve anahtar + değer bileşenleri yapısı  kullanılarak e-ticaret e-arşiv kurallarına uygun belge hazırlama örneği

---
+++ Electronic/NetleEFatura/E-Ticaret Kullanım Örneği.cs
@@ -0,0 +1,20 @@
+var netleEfatura = new NetleEFatura();
+
+///diğer tamamlayıcı bilgiler
+
+nef.EkSahalar = new EArchiveWS.EkSaha[]
+{
+    new EArchiveWS.EkSaha(){Anahtar="Enable_B2C", Deger="1"},
+    new EArchiveWS.EkSaha(){Anahtar="EArc_B2C_Field_WebSite", Deger="http://www.netle.com.tr"},
+    new EArchiveWS.EkSaha(){Anahtar="EArc_B2C_Field_PaymentType", Deger="Nakit"},
+    new EArchiveWS.EkSaha(){Anahtar="EArc_B2C_Field_PaymentDate", Deger="01.01.2015, cuma"},
+    new EArchiveWS.EkSaha(){Anahtar="EArc_B2C_Field_Transport_Person_Name", Deger="Necati Salıgil"},
+    new EArchiveWS.EkSaha(){Anahtar="EArc_B2C_Field_Transport_Person_TCKN", Deger="1501478851"},
+    new EArchiveWS.EkSaha(){Anahtar="EArc_B2C_Field_Transport_Company_Title", Deger="Netle Yazılım"},
+    new EArchiveWS.EkSaha(){Anahtar="EArc_B2C_Field_Transport_Company_VKN", Deger="1818181818"},
+    new EArchiveWS.EkSaha(){Anahtar="EArc_B2C_Field_Dispatch_Date", Deger="01.01.2015"},
+    new EArchiveWS.EkSaha(){Anahtar="EArc_B2C_Field_Dispatch_Time", Deger="10:00"},
+
+    new EArchiveWS.EkSaha(){Anahtar="Enable_Dispatch", Deger="1"},
+    new EArchiveWS.EkSaha(){Anahtar="Enable_Electronic", Deger="1"}
+};
